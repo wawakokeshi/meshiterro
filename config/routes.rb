36c708412896a071d19a73e96f98311deb0b004a
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-01-31 13:09:31 +0000

    [4-10]Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   devise_for :users
   #devise_for :users は、devise を使用する際に URL として users を含むことを示しています。
-  root to: "homes#top"
+
+  resources :post_images, only: [:new, :index, :show]
+
+
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
