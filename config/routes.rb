2521ce4b8ca16cf3ab8de5aacce61fc99d9357aa
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-02-01 12:42:46 +0000

    [4-10]Finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   #devise_for :users は、devise を使用する際に URL として users を含むことを示しています。
 
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
 
   get 'homes/about' => 'homes#about', as: 'about'
